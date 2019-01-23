0dd35d4d52e1f0e3717082a8c8e9f3e00886f521
ArthurWZH <32083090+ArthurWZH@users.noreply.github.com>
2019-01-23 09:45:49 +0800

    previous update

---
+++ CalenderPlan/Words.swift
@@ -0,0 +1,15 @@
+//
+//  Words.swift
+//  CalenderPlan
+//
+//  Created by Zihao Arthur Wang [STUDENT] on 11/13/18.
+//  Copyright © 2018 Zihao Arthur Wang [STUDENT]. All rights reserved.
+//
+
+import Foundation
+
+class Words {
+    static let chinese = "Chinese"
+    static let english = "English"
+    static let edit = ["Chinese": "编辑", "English": "Edit"]
+}
