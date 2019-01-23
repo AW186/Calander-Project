7376d0de45b3cecb4a70ca525a37299a01ead3b8
ArthurWZH <32083090+ArthurWZH@users.noreply.github.com>
2019-01-23 09:45:37 +0800

    previous update

---
+++ CalenderPlan/ColorBoard.swift
@@ -0,0 +1,18 @@
+//
+//  ColorBoard.swift
+//  CalenderPlan
+//
+//  Created by Zihao Arthur Wang [STUDENT] on 10/16/18.
+//  Copyright © 2018 Zihao Arthur Wang [STUDENT]. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class ColorBoard {
+    static let borderColor1: CGColor = NSColor.brown.cgColor
+    static let borderColor2: CGColor = NSColor.darkGray.cgColor
+    static let textColor1: CGColor = NSColor.black.cgColor
+    static let textColor2: CGColor = CGColor.init(red: 0.3, green: 0.28, blue: 0.2, alpha: 1)
+    static let tintTextColor: CGColor = NSColor.red.cgColor
+}
